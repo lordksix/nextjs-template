2e28f25a86596da78f3ae596ec1b03a03094c132
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-12-27 14:00:26 -0500

    Check new husky config

--- app/page.tsx
+++ app/page.tsx
@@ -105,6 +105,7 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Awesome!
           </p>
         </a>
       </div>
