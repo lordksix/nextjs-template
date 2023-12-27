4f4d8926daa43f037e992fb9c0fe7eb7ab930382
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-12-27 12:50:47 -0500

    restructure folder

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: './'
+});
+
+/** @type {import('jest').Config} */
+const config = {
+  collectCoverageFrom: [
+    "app/**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+  ],
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  preset: 'ts-jest'
+};
+
+export default createJestConfig(config);
