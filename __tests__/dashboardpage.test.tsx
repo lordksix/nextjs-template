d13a2f4e274ce946e4c09cce724c4709363a13b6
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-12-27 13:57:33 -0500

    Imrpove tsconfig

---
+++ __tests__/dashboardpage.test.tsx
@@ -0,0 +1,12 @@
+import Home from '@/app/page';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+describe.skip('Dashboard Component Render', () => {
+  it('should render properly', () => {
+    render(<Home />);
+    const myText: HTMLElement = screen.getByText(/tsx/i);
+
+    expect(myText).toBeInTheDocument();
+  });
+});
